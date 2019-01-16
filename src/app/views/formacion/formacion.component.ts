3f14d642d76aef5305d13d3027ddefe8f215278a
Luis Alberto <disconluis@gmail.com>
2019-01-16 15:49:38 +0100

    fixed header, set main color and second color

--- src/app/views/formacion/formacion.component.ts
+++ src/app/views/formacion/formacion.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-formacion',
   templateUrl: './formacion.component.html',
-  styleUrls: ['./formacion.component.css']
+  styleUrls: ['./formacion.component.css'],
 })
 export class FormacionComponent implements OnInit {
 
