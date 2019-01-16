9cf8f7bdd8d6aaccd5e324e309ca050b268035df
Luis Alberto <disconluis@gmail.com>
2019-01-16 23:36:27 +0100

    gallery Done! with artisan modal

---
+++ src/app/models/galeria.ts
@@ -0,0 +1,32 @@
+export const ImagesBasic = [
+  {
+    img: "assets/img/galeria1.jpg",
+    id: "1",
+    description: "Realizando un testing a las Memorias RAM."
+  },
+  {
+    img: "assets/img/galeria2.jpg",
+    id: "2",
+    description: "Realizando un testing de tarjetas gráficas."
+  },
+  {
+    img: "assets/img/galeria3.jpg",
+    id: "3",
+    description: "Realizando un testing de una Placa Base."
+  },
+  {
+    img: "assets/img/galeria4.jpg",
+    id: "4",
+    description: "Limpieza de un Ordenador"
+  },
+  {
+    img: "assets/img/galeria5.jpg",
+    id: "5",
+    description: 'Lugar de trabajo (Montaje, Configuración y Testing de Ordenadores)'
+  },
+  {
+    img: "assets/img/galeria6.jpg",
+    id: "6",
+    description: 'Limpieza y formateo de Discos Duros'
+  }
+];
--- src/app/views/galeria/galeria.component.ts
+++ src/app/views/galeria/galeria.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ImagesBasic } from 'src/app/models/galeria';
 
 @Component({
   selector: 'app-galeria',
@@ -7,9 +8,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GaleriaComponent implements OnInit {
 
+  imagesBasic : any[] = ImagesBasic
+  image_modal : string = ""
+  image_description: string = ""
+  id : string = ""
+
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit() { }
+
+  onOpen(event: any) {
+    this.image_modal = event.target.src
+
   }
 
+  onOpenImg(id: string) {
+    this.id = id
+    console.log(this.id)
+    this.getDescription(this.id)
+  }
+
+  getDescription(id: string) {
+    this.imagesBasic.forEach(elem => {
+      if(elem.id == id) this.image_description = elem.description
+    });
+  }
 }
