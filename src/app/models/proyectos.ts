5393c2b08f41431b6d1e705adb4b5845a3086c27
@LuisArana <disconluis@gmail.com>
2020-04-27 14:55:57 +0200

    skip some projects

--- src/app/models/proyectos.ts
+++ src/app/models/proyectos.ts
@@ -1,68 +1,72 @@
 export const Proyectos = [
   {
-    color: 'pink',
-    icon: 'desktop',
-    icon_title: 'Web Design',
-    title: 'Cafetería Dolç Café',
-    image: 'assets/img/cafe.jpg',
-    description: 'Diseño, publicación y desarrollo de la página web para esta cafeteria, instalación de plugins y aprovechando al máximo el potencial de WORDPRESS.',
-    url: 'https://dolçcafe.com',
-    btn_icon: 'clone',
-    btn_txt: 'VER PROYECTO'
+    color: "pink",
+    icon: "desktop",
+    icon_title: "Web Design",
+    title: "Cafetería Dolç Café",
+    image: "assets/img/cafe.jpg",
+    description:
+      "Diseño, publicación y desarrollo de la página web para esta cafeteria, instalación de plugins y aprovechando al máximo el potencial de WORDPRESS.",
+    url: "https://dolçcafe.com",
+    btn_icon: "clone",
+    btn_txt: "VER PROYECTO",
   },
   {
-    color: 'orange',
-    icon: 'desktop',
-    icon_title: 'Web Design',
-    title: 'Pizzería ÑamÑam',
-    image: 'assets/img/pizzeria.jpg',
-    description: 'Diseño, publicación y desarrollo de la pàgina web para esta pizzería, con el potente FrameWork Angular 7, utilizando el patron de diseño MVC (Modelo, Vista, Controlador)',
-    url: 'https://pizzeriafamiliarñamñam.com',
-    btn_icon: 'clone',
-    btn_txt: 'VER PROYECTO'
+    color: "orange",
+    icon: "desktop",
+    icon_title: "Web Design",
+    title: "Pizzería ÑamÑam",
+    image: "assets/img/pizzeria.jpg",
+    description:
+      "Diseño, publicación y desarrollo de la pàgina web para esta pizzería, con el potente FrameWork Angular 7, utilizando el patron de diseño MVC (Modelo, Vista, Controlador)",
+    url: "https://pizzeriafamiliarñamñam.com",
+    btn_icon: "clone",
+    btn_txt: "VER PROYECTO",
   },
   {
-    color: 'blue',
-    icon: 'code',
-    icon_title: 'Development & Debugging',
-    title: 'Barco de Alquiler',
-    image: 'assets/img/boat.png',
-    description: 'Debugging de la web al añadir nuevas funcionalidades a la página web, como nuevos barco de https://www.booking-manager.com/, utilizando PHP 7 y ZURB Foundation 6.3.1.',
-    url: 'https://barcodealquiler.com',
-    btn_icon: 'clone',
-    btn_txt: 'VER PROYECTO'
+    color: "blue",
+    icon: "code",
+    icon_title: "Development & Debugging",
+    title: "Barco de Alquiler",
+    image: "assets/img/boat.png",
+    description:
+      "Debugging de la web al añadir nuevas funcionalidades a la página web, como nuevos barco de https://www.booking-manager.com/, utilizando PHP 7 y ZURB Foundation 6.3.1.",
+    url: "https://barcodealquiler.com",
+    btn_icon: "clone",
+    btn_txt: "VER PROYECTO",
   },
   {
-    color: 'green',
-    icon: 'code',
-    icon_title: 'Development & Debugging',
-    title: 'Onboard Yacht Services',
-    image: 'assets/img/obys.jpg',
-    description: 'Se añadió a la WEB www.barcodealquiler.com todos los servicios de onboardyachtservices al manage, utilizando PHP 7, PDO y ZURB Foundation 6.3.1.',
-    url: 'http://www.onboardyachtservices.com/',
-    btn_icon: 'clone',
-    btn_txt: 'VER PROYECTO'
+    color: "green",
+    icon: "code",
+    icon_title: "Development & Debugging",
+    title: "Onboard Yacht Services",
+    image: "assets/img/obys.jpg",
+    description:
+      "Se añadió a la WEB www.barcodealquiler.com todos los servicios de onboardyachtservices al manage, utilizando PHP 7, PDO y ZURB Foundation 6.3.1.",
+    url: "http://www.onboardyachtservices.com/",
+    btn_icon: "clone",
+    btn_txt: "VER PROYECTO",
   },
-  {
-    color: 'yellow',
-    icon: 'code',
-    icon_title: 'Development & Debugging',
-    title: 'CRM Metro-Electrónica',
-    image: 'assets/img/metro.jpg',
-    description: 'Se implemento todos los servicios y funcionalidades para que este proyecto administre y organice la empresa, desde facturación, reparaciones y control de stocks.',
-    url: 'http://metro.host20.net/',
-    btn_icon: 'clone',
-    btn_txt: 'VER PROYECTO'
-  },
-  {
-    color: 'pink',
-    icon: 'code',
-    icon_title: 'Development & Debugging',
-    title: 'Sistema de Monitorización y Alertas',
-    image: 'assets/img/simona.jpg',
-    description: 'SIMONA es una sistema de monitorización y alertas, he desarrollado todo el frontEnd del sistema y parte del backEnd.',
-    url: 'http://simona.land/',
-    btn_icon: 'clone',
-    btn_txt: 'VER PROYECTO'
-  }
-]
+  // {
+  //   color: 'yellow',
+  //   icon: 'code',
+  //   icon_title: 'Development & Debugging',
+  //   title: 'CRM Metro-Electrónica',
+  //   image: 'assets/img/metro.jpg',
+  //   description: 'Se implemento todos los servicios y funcionalidades para que este proyecto administre y organice la empresa, desde facturación, reparaciones y control de stocks.',
+  //   url: 'http://metro.host20.net/',
+  //   btn_icon: 'clone',
+  //   btn_txt: 'VER PROYECTO'
+  // },
+  // {
+  //   color: 'pink',
+  //   icon: 'code',
+  //   icon_title: 'Development & Debugging',
+  //   title: 'Sistema de Monitorización y Alertas',
+  //   image: 'assets/img/simona.jpg',
+  //   description: 'SIMONA es una sistema de monitorización y alertas, he desarrollado todo el frontEnd del sistema y parte del backEnd.',
+  //   url: 'http://simona.land/',
+  //   btn_icon: 'clone',
+  //   btn_txt: 'VER PROYECTO'
+  // }
+];
